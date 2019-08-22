e17a3d04882482879d45774cd478192040aaf0f9
yys8646 <yys8646@gmail.com>
2019-08-22 17:11:18 +0900

    version 3.7.4-v0.1.0

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ apt-get update && \
 apt-get install python3.5 && \
 apt-get install python3.6 && \
 apt-get install python3.7
+
+apt-get install -y wget && \
+wget https://bootstrap.pypa.io/get-pip.py && \
+/usr/bin/python3 get-pip.py && \
+rm get-pip.py
